74cc88b7a4644b544c06f21ac9698209ce8e85f4
wilcox-liam <wilcox.liam@gmail.com>
2020-03-19 19:00:05 -0300

    Day 4

---
+++ 2019/day4/p1.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	password_count := 0
+
+	dat, err := ioutil.ReadFile("input.txt")
+	check(err)
+	input_range := string(dat)
+
+	passwords := strings.Split(input_range, "-")
+	password_low, err := strconv.Atoi(passwords[0])
+	check(err)
+	password_high, err := strconv.Atoi(passwords[1])
+	check(err)
+
+	fmt.Println("LOW:", password_low, "HIGH:", password_high)
+	for ; password_low <= password_high; password_low++ {
+		if (is_six_digits(password_low) && contains_number_pair(password_low) && numbers_increase(password_low)) {
+			password_count++		
+		}
+	} 
+	fmt.Println(password_count)
+}
+
+//Check that the password is 6 characters long
+func is_six_digits(password int) bool {
+	pass := strings.Split(strconv.Itoa(password), "")
+
+	if len(pass) == 6 {
+		return true
+	}
+	return false
+}
+
+//Checks a string of numbers whether it contains a pair of adjacent identical numbers
+func contains_number_pair(password int) bool {
+	pass := strings.Split(strconv.Itoa(password), "")
+
+	//Start at 1 and compare to previous, end 
+	for i := 1; i < len(pass); i++ {
+		if pass[i] == pass[i-1] {		
+			return true
+		}
+	}
+	return false
+}
+
+//Checks a string of numbers, that each individual number is more than or equal to the previous
+func numbers_increase(password int) bool {
+	pass := strings.Split(strconv.Itoa(password), "")
+
+	//Start at 1 and compare to previous, end 
+	for i := 1; i < len(pass); i++ {
+		if pass[i] < pass[i-1] {
+			return false
+		}
+	}
+	return true
+}
---
+++ 2019/day4/p2.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	password_count := 0
+
+	dat, err := ioutil.ReadFile("input.txt")
+	check(err)
+	input_range := string(dat)
+
+	passwords := strings.Split(input_range, "-")
+	password_low, err := strconv.Atoi(passwords[0])
+	check(err)
+	password_high, err := strconv.Atoi(passwords[1])
+	check(err)
+
+	fmt.Println("LOW:", password_low, "HIGH:", password_high)
+	for ; password_low <= password_high; password_low++ {
+		if (is_six_digits(password_low) && contains_number_pair(password_low) && numbers_increase(password_low)) {
+			fmt.Println(password_low)
+			password_count++		
+		}
+	} 
+	fmt.Println(password_count)
+}
+
+//Check that the password is 6 characters long
+func is_six_digits(password int) bool {
+	pass := strings.Split(strconv.Itoa(password), "")
+
+	if len(pass) == 6 {
+		return true
+	}
+	return false
+}
+
+//Checks a string of numbers whether it contains a pair of adjacent identical numbers
+//Does not return true if there is more than a pair of identical numbers. e.g 3 x 2 (222)
+func contains_number_pair(password int) bool {
+	pass := strings.Split(strconv.Itoa(password), "")
+	pair := false
+	match_count := 0
+
+	//Start at 1 and compare to previous
+	for i := 1; i < len(pass); i++ {
+		if pass[i] == pass[i-1] {	
+			match_count += 1
+			if pair {
+				pair = false
+			} else {
+				if match_count == 1 {
+					pair = true	
+				}
+			}
+		} else {
+			if pair {
+				return true
+			}
+			match_count = 0
+		}
+	}
+	return pair
+}
+
+//Checks a string of numbers, that each individual number is more than or equal to the previous
+func numbers_increase(password int) bool {
+	pass := strings.Split(strconv.Itoa(password), "")
+
+	//Start at 1 and compare to previous, end 
+	for i := 1; i < len(pass); i++ {
+		if pass[i] < pass[i-1] {
+			return false
+		}
+	}
+	return true
+}
